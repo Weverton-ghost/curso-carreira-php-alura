846fa07277ca4e2b29345c29fb4c89605914edd6
Weverton-ghost <wevertonlrvinacio2@gmail.com>
2026-01-21 13:53:18 -0300

    PHP - Aprendendo sobre JSON encode e decode.

--- Screen-match-minha-versão/melhores-assistidos.php
+++ Screen-match-minha-versão/melhores-assistidos.php
@@ -3,7 +3,7 @@
 //Função para exibir mensagem.
 //O tipo de retorno é o void, pois ele não retornou nada, ficou no vácuo, a function não teve um tipo de retorno para a variável pela qual foi puxada. 
 
-require __DIR__ . "/../Avaliações-filmes/melhores-avaliados.php";
+require __DIR__ . "/melhores-avaliados.php";
 //Com o DIR eu transformo de relativo que pode mudar, para um caminho absoluto, evitando bugs futuros.
 //é a mesma função ele copia a função se ela tivesse lá em mehlores-avaliados.php, porque para o teste eu colei ela lá 
 
@@ -54,6 +54,19 @@ $filme = [
     "Nota do filme" => 7.8,
     "Gênero do filme" => "Super-herói",
 ]; 
+/*Imagine que eu queira exportar tudo o que está dentro da variável $filme para que em outro computador eu poderia importar também, ou até mesmo importar para a minha interface web, para alguém salvar, fazer um upload, enviar para um amigo, basicamente um formato que eu possa manipular via código.
+
+E sobre a maniuplação precisamos entender os formatos de dados que podemos ultilizar, nesse caso queremos importar de forma que possamos importar também ou seja, quero escrever salvar esse arquivo, e depois ler e entender essas informações de volta.
+
+E para isso vamos utilizar o JSON para empacotar esses dados desse arrays da variável $filme, tranformando em JSON ser tornará algo que possa ser armazenado, salvo em um arquivo, lido pelo programa depois...*/ 
+
+//echo json_encode($filme);
+//Essa função pega um valor ou variável e transforma/empacota no formato JSON
+
+//Agora imagine que queremos transformar ela em algo que o meu php entenda, e não fique igual está {"nome":"Thor: Ragnarok","ano":2021,"nota":7.8,"genero":"super-her\u00f3i"} 
+
+var_dump(json_decode('{"nome":"Thor: Ragnarok","ano":2021,"nota":7.8,"genero":"super-her\u00f3i"}', true));
+//Passo true por parametro, porque ele me pergunta, você quer que ele continue seguindo o mesmo modelo = array associativo, se eu não passo esse parametro ele se torna um objeto. 
 
 //echo $filme[2];
 //Percebe que imprimiu apenas a nota que é o 2, porque começa em 0;
